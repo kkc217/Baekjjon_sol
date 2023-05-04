695f0db3921e014df104a50d0a2b123cd4168e0d
kkc217 <kkc217@naver.com>
2023-05-04 18:04:59 +0900

    Q11660 complete!

---
+++ baekjoon/java/src/Q11660.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Q11660 {
+
+    public static int n;
+    public static int m;
+    public static int[][] matrix;
+    public static int[][] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        matrix = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= n; j++) {
+                matrix[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        makeDp();
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+            bw.write(getSum(x1, y1, x2, y2) + "\n");
+        }
+
+        bw.flush();
+    }
+
+    public static void makeDp() {
+        dp = new int[n + 1][n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + matrix[i][j];
+            }
+        }
+    }
+
+    public static int getSum(int x1, int y1, int x2, int y2) {
+        return dp[x2][y2] - dp[x1 - 1][y2] - dp[x2][y1 - 1] + dp[x1 - 1][y1 - 1];
+    }
+
+}
